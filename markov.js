a8c5900f13e7fc96e4cc15b00444f3b53ec392a9
Vaughn Seekamp <vseekamp@gmail.com>
2023-04-04 17:16:20 -0700

    getText bug with while loop

--- markov.js
+++ markov.js
@@ -15,7 +15,7 @@ class MarkovMachine {
   /** Get markov chain: returns Map of Markov chains.
    *
    *  For text of "The cat in the hat.", chains will be:
-   * 
+   *
    *  {
    *   "The": ["cat"],
    *   "cat": ["in"],
@@ -23,11 +23,27 @@ class MarkovMachine {
    *   "the": ["hat."],
    *   "hat.": [null],
    *  }
-   * 
+   *
    * */
-
+  // Loop over input text
+  // if the word is not a key in the object,
+  // add it as a key and add the next word as a value.
+  // if key in object
+  // consider the null scenario
   getChains() {
     // TODO: implement this!
+    let wordChain = {};
+    for (let i = 0; i < this.words.length; i++) {
+      if (wordChain[this.words[i]] === undefined) {
+        wordChain[this.words[i]] = [];
+      }
+      if (i === this.words.length - 1) {
+        wordChain[this.words[i]].push(null);
+      } else {
+        wordChain[this.words[i]].push(this.words[i + 1]);
+      }
+    }
+    return wordChain;
   }
 
 
@@ -38,7 +54,26 @@ class MarkovMachine {
     // TODO: implement this!
 
     // - start at the first word in the input text
-    // - find a random word from the following-words of that
+    // - find a random word from the following-words of that markovChain
     // - repeat until reaching the terminal null
+    let markovChain = [this.words[0]];
+
+    while (true) {
+      let word = randomFrom(markovChain[markovChain.length-1]);
+      if (word === null) {
+        break;
+      } else {
+        markovChain.push(word);
+      }
+    }
+  return markovChain;
   }
 }
+
+function randomFrom(array) {
+  return array[Math.floor(Math.random()*array.length)];
+}
+
+let text = new MarkovMachine("I am a very pretty little cat. Cat is a pretty wild animal.");
+
+console.log(text.getText());
